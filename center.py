49da2d92cc36352355d0945fa8533cf07e4bac68
vyshnavimuthumula <38848887+vyshnavimuthumula@users.noreply.github.com>
2018-05-21 20:16:51 +0530

    Update center.py

--- center.py
+++ center.py
@@ -1,20 +1,36 @@
-#!/usr/bin/env python
-print("Content-type:text/html \n")
 import media
 import fresh_tomatoes
 
-a = media.Movie("Wall-e","Robot left on earth","https://images-na.ssl-images-amazon.com/images/I/51RoZRgIHtL.jpg"
-                    ,"https://www.youtube.com/embed/ZisWjdjs-gM")
-b=media.Movie("Harrypoter","dadlove"
-              ,"https://vignette.wikia.nocookie.net/harrypotter/images/6/65/Harry-Potter-and-the-Deathly-Hallows-Part-1-poster.jpg/revision/latest?cb=20101001182826"
-              ,"https://www.youtube.com/embed/Jt1Nv-8_-cg")
-c=media.Movie("Bagamathi"," story of Queen Bhaagamathie","https://bestoftheyear.in/wp-content/uploads/2018/02/Bhaagamathie-1-400x567.jpg"
-              ,"https://www.youtube.com/embed/Aahj3atxdS4")
-d=media.Movie("Inception","In dream story","https://images-na.ssl-images-amazon.com/images/I/61Ug%2BK8o5FL.jpg"
-              ,"https://www.youtube.com/embed/d3A3-zSOBT4")
-e=media.Movie("Toliprema","firstlove","https://www.comingtrailer.com/images/poster/Tholi-Prema7.jpg"
-                    ,"https://www.youtube.com/embed/-kFvrsAgp3M")
-f=media.Movie("Bahubali2","old","https://images-na.ssl-images-amazon.com/images/I/61fhQI78YpL._SY550_.jpg","http://www.youtube.com/embedded/G62HrubdD6o")
+walle = media.Movie(
+  "Wall-e",
+  "Robot left on earth",
+  "https://images-na.ssl-images-amazon.com/images/I/51RoZRgIHtL.jpg",
+  "https://www.youtube.com/embed/ZisWjdjs-gM")
+harrypoter = media.Movie(
+  "Harrypoter",
+  "story of a boy",
+  "https://bit.ly/2IDio0E",
+  "https://www.youtube.com/embed/Jt1Nv-8_-cg")
+bagamathi = media.Movie(
+  "Bagamathi",
+  " story of Queen Bhaagamathie",
+  "https://bit.ly/2IYoV5F",
+  "https://www.youtube.com/embed/Aahj3atxdS4")
+inception = media.Movie(
+  "Inception",
+  "In dream story",
+  "https://images-na.ssl-images-amazon.com/images/I/61Ug%2BK8o5FL.jpg",
+  "https://www.youtube.com/embed/d3A3-zSOBT4")
+toliprema = media.Movie(
+  "Toliprema",
+  "firstlove",
+  "https://www.comingtrailer.com/images/poster/Tholi-Prema7.jpg",
+  "https://www.youtube.com/embed/-kFvrsAgp3M")
+bahubali2 = media.Movie(
+  "Bahubali2",
+  "old",
+  "https://images-na.ssl-images-amazon.com/images/I/61fhQI78YpL._SY550_.jpg",
+  "http://www.youtube.com/embedded/G62HrubdD6o")
 
-movies = [a,b,c,d,e,f]
+movies = [walle, harrypoter, bagamathi, inception, toliprema, bahubali2]
 fresh_tomatoes.open_movies_page(movies)
