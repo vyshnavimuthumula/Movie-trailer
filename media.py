778a50e80a47324aebbd5e9f60323055a26f5f36
vyshnavimuthumula <38848887+vyshnavimuthumula@users.noreply.github.com>
2018-05-22 01:24:28 +0530

    Update media.py

--- media.py
+++ media.py
@@ -7,19 +7,19 @@ class Movie():
     VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
 
     def __init__(self,
-                 movie_title, movie_storyline, poster_image, trailer_yout):
+                 movie_title, m_storyline, poster_image, trailer_youtube):
         """ Inits a Movie object
         Arguments:
         movie_title = a string of the movie's title
-        movie_storyline = summery of the movie
+        m_storyline = summery of the movie
         poster_image = a string containing a URL to a poster image
         trailer_yout = a string containing a youtube URL of the movie's trailer
         """
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.storyline = m_storyline
         self.poster_image_url = poster_image
-        self.trailer_yout_url = trailer_yout
+        self.trailer_yout_url = trailer_youtube
 
     def show_trailer(self):
         """ Opens trailer in a web browser """
-        webbrowser.open(self.trailer_yout_url)
+        webbrowser.open(self.trailer_youtube_url)
