273223aac4457cc83ef78de04ae584e477a064f7
vyshnavimuthumula <38848887+vyshnavimuthumula@users.noreply.github.com>
2018-05-21 22:49:18 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,14 +2,24 @@ import webbrowser
 
 
 class Movie():
+    """ Class for representing a movie """
+
     VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
 
     def __init__(self,
-                 movie_title, movie_storyline, poster_image, trailer_youtube):
+                 movie_title, movie_storyline, poster_image, trailer_yout):
+        """ Inits a Movie object
+        Args:
+        movie_title = a string of the movie's title
+        movie_storyline = summery of the movie
+        poster_image = a string containing a URL to a poster image
+        trailer_yout = a string containing a youtube URL of the movie's trailer
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.trailer_yout_url = trailer_youtube
 
     def show_trailer(self):
+        """ Opens trailer in a web browser """
         webbrowser.open(self.trailer_youtube_url)
